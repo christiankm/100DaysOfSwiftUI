d2fc83b686e1342a622f94d4a520434a459b329b
Christian Mitteldorf <christian.km@me.com>
2019-11-23 17:05:55 +0100

    Complete Day 53

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Christian Mitteldorf on 23/11/2019.
+//  Copyright © 2019 Mitteldorf. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    @Environment(\.managedObjectContect) var moc
+
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+
+    var body: some View {
+        VStack {
+            List {
+                ForEach(students, id: \.id) { student in
+                    Text(student.name ?? "Unknown")
+                }
+            }
+
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+
+                let student = Student(context: self.moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+
+                try? self.moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
