da3d42be931f84ab0145c68106321a0fa1fd3086
Christian Mitteldorf <cmit@danskebank.dk>
2019-11-24 16:32:41 +0100

    Complete Day 62

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Christian Mitteldorf on 24/11/2019.
+//  Copyright © 2019 mitteldorf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
+    @State private var blurAmount: CGFloat = 0 {
+        didSet {
+            print("New value is \(blurAmount)")
+        }
+    }
+
+    var body: some View {
+        let blur = Binding<CGFloat>(
+            get: {
+                self.blurAmount
+            },
+            set: {
+                self.blurAmount = $0
+                print("New value is \(self.blurAmount)")
+            })
+
+        return VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+                .background(backgroundColor)
+                .onTapGesture {
+                    self.showingActionSheet = true
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                    .default(Text("Red")) { self.backgroundColor = .red },
+                    .default(Text("Green")) { self.backgroundColor = .green },
+                    .default(Text("Blue")) { self.backgroundColor = .blue },
+                    .cancel()
+                ])
+            }
+
+            Slider(value: blur, in: 0...20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
