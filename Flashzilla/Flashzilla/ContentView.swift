ae8d777e3d8a96183117fea1b3524437e90aceaf
Christian Mitteldorf <christian.km@me.com>
2019-12-19 14:22:26 +0100

    Complete Day 87

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Christian Mitteldorf on 19/12/2019.
+//  Copyright © 2019 Mitteldorf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
